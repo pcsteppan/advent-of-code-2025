f858cff6150f96fc78dc5dcaca4f06fccabcbda0
Patrick Steppan <patrick.steppan@gmail.com>
2025-12-07 10:53:27 -0600

    day 6 cleanup

--- 6_go/main.go
+++ 6_go/main.go
@@ -8,8 +8,6 @@ import (
 	"strings"
 )
 
-const _example = "123 328  51 64 \r\n 45 64  387 23 \r\n  6 98  215 314\r\n*   +   *   +  \r\n"
-
 func main() {
 	input := readFileToStr("input.txt")
 	part1(input)
@@ -17,40 +15,30 @@ func main() {
 }
 
 func part1(input string) {
-	lines := slices.Collect(strings.Lines(input))
+	lines, ops := parseInput(input)
 	results := parseInts(strings.Fields(lines[0]))
-	op_line := strings.Fields(lines[len(lines)-1])
-	lines = lines[1 : len(lines)-1]
+	lines = lines[1:]
 
 	for _, line := range lines {
 		nums := parseInts(strings.Fields(line))
+
 		for i, num := range nums {
-			if op_line[i] == "*" {
-				results[i] *= num
-			} else {
-				results[i] += num
-			}
+			results[i] = applyOp(
+				ops[i],
+				results[i],
+				num)
 		}
 	}
 
-	sum := 0
-	for _, n := range results {
-		sum += n
-	}
+	sum := sum(results)
 	fmt.Printf("part 1: %d\n", sum)
 }
 
 func part2(input string) {
 	vertical_nums := getVerticalNums(input)
-	lines := slices.Collect(strings.Lines(input))
-	op_line := strings.Fields(lines[len(lines)-1])
+	_, ops := parseInput(input)
 
-	group_count := 1
-	for _, n := range vertical_nums {
-		if n == 0 {
-			group_count += 1
-		}
-	}
+	group_count := len(ops)
 
 	subtotals := make([]int, group_count)
 	group_idx := 0
@@ -60,45 +48,21 @@ func part2(input string) {
 			continue
 		}
 
-		if op_line[group_idx] == "*" {
-			if subtotals[group_idx] == 0 {
-				subtotals[group_idx] = 1
-			}
-
-			subtotals[group_idx] *= num
-		} else {
-			subtotals[group_idx] += num
-		}
+		subtotals[group_idx] = applyOp(
+			ops[group_idx],
+			subtotals[group_idx],
+			num)
 	}
 
-	sum := 0
-	for _, num := range subtotals {
-		sum += num
-	}
-
+	sum := sum(subtotals)
 	fmt.Printf("part 2: %d", sum)
-}
-
-func readFileToStr(fname string) string {
-	data, _ := os.ReadFile(fname)
-	return string(data)
-}
-
-func parseInts(num_strs []string) []int {
-	nums := make([]int, len(num_strs))
-	for i, str := range num_strs {
-		nums[i], _ = strconv.Atoi(str)
-	}
-	return nums
 }
 
 // returns array of ints that represent the vertical nums
 // in the input, and where 0s represent empty columns
 func getVerticalNums(table string) []int {
-	data := strings.ReplaceAll(table, "\r\n", "\n")
-
-	width := strings.Index(data, "\n")
-	height := strings.Count(data, "\n") - 1
+	width := strings.Index(table, "\n")
+	height := strings.Count(table, "\n") - 1
 
 	nums := make([]int, width)
 
@@ -106,7 +70,7 @@ func getVerticalNums(table string) []int {
 		sum := 0
 		for row := range height {
 			idx := row*(width+1) + col
-			ch := data[idx]
+			ch := table[idx]
 
 			var n int
 			if ch == byte(' ') {
@@ -121,5 +85,48 @@ func getVerticalNums(table string) []int {
 		nums[col] = sum
 	}
 
+	return nums
+}
+
+//////////
+// helpers
+
+func parseInput(input string) (lines []string, ops []string) {
+	lines = slices.Collect(strings.Lines(input))
+	ops = strings.Fields(lines[len(lines)-1])
+	lines = lines[:len(lines)-1]
+	return lines, ops
+}
+
+func sum(nums []int) int {
+	total := 0
+	for _, n := range nums {
+		total += n
+	}
+	return total
+}
+
+func applyOp(op string, acc int, val int) int {
+	if op == "*" {
+		if acc == 0 {
+			acc = 1
+		}
+		return acc * val
+	}
+
+	// otherwise '+'
+	return acc + val
+}
+
+func readFileToStr(fname string) string {
+	data, _ := os.ReadFile(fname)
+	return string(data)
+}
+
+func parseInts(num_strs []string) []int {
+	nums := make([]int, len(num_strs))
+	for i, str := range num_strs {
+		nums[i], _ = strconv.Atoi(str)
+	}
 	return nums
 }
